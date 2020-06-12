304f059dfa7646a6c881a0d4dcae484deef2a602
snake-whisper <snake-whisper@web-utils.eu>
2020-06-12 18:50:18 +0000

    implement basic mailManager

--- lib/mailManager.php
+++ lib/mailManager.php
@@ -3,27 +3,58 @@ error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
 require_once "Mail.php";
 class MailManager
 {
-    function __construct () {
+    function __construct ()
+    {
         $this->config = require ("../config.php");
         $this->smtp = Mail::factory('smtp', array ('host' => $this->config['smtp']['host'],
                                     'port' => $this->config['smtp']['port'],
-                                    'auth' => true,
+                                    'auth' => $this->config['smtp']['auth'],
                                     'username' => $this->config['smtp']['username'],
-                                    'password' => $this->config['smtp']['password']));
-        
+                                    'password' => $this->config['smtp']['password']));        
     }
 
-    function sendToken($token, $to, $reason) {
-        $headers = array ('From' => $this->config['smtp']['username'], 'To' => $to, 'Subject' => "Please Verify your", 'Reply-To' => $this->config['smtp']['username']);
-        $this->smtp->send($to, $headers, "Please verify the toke " . $token);
+    private function chkError($ret)
+    {
+        if (PEAR::isError($ret)) {
+            echo "<p>" . $ret->getMessage() . "</p>\n";
+        } else {
+            echo "<p>Message successfully sent!</p>\n";
+        }
+    }
+
+    public function sendRegisterToken(String $token, String $to)
+    {
+        $headers = array ('From' => $this->config['smtp']['username'],
+                            'To' => $to,
+                            'Subject' => "Please Verify your Registration");
+        $mail = require ("templates/register.template");
+        $ret = $this->smtp->send($to, $headers, $mail);
+        $this->chkError($ret);
+    }
+
+    public function sendSubscribeToken(String $token, String $to, String $distName)
+    {
+        $headers = array ('From' => $this->config['smtp']['username'],
+                            'To' => $to,
+                            'Subject' => "Please Verify your Subscription");
+        $mail = require ("templates/subscribe.template");
+        $ret = $this->smtp->send($to, $headers, $mail);
+        $this->chkError($ret);
+    }
+
+    public function sendUnSubscribeToken(String $token, String $to, String $distName)
+    {
+        $headers = array ('From' => $this->config['smtp']['username'],
+                            'To' => $to,
+                            'Subject' => "Please Verify your Unsubscription");
+        $mail = require ("templates/unsubscribe.template");
+        $ret = $this->smtp->send($to, $headers, $mail);
+        $this->chkError($ret);
     }
 }
 
-$mail = new MailManager();
-$mail->sendToken("1d56v1d5f", "verf@web-utils.eu", "Registration");
-if (PEAR::isError($mail)) {
-    echo("<p>" . $mail->getMessage() . "</p>");
-    } else {
-    echo("<p>Message successfully sent!</p>");
-    }
+//$mail = new MailManager();
+//$mail->sendRegisterToken("1d56v1d5f", "verf@web-utils.eu");
+//$mail->sendSubscribeToken("vbdsvdfsg", "verf@web-utils.eu", "KartoffelWG");
+
 ?>
