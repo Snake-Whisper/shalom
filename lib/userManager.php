08ee2bf4ac3178915a90961e0dbc5b3bae74f886
snake-whisper <snake-whisper@web-utils.eu>
2020-06-13 21:13:35 +0200

    sync

--- lib/userManager.php
+++ lib/userManager.php
@@ -1,12 +1,22 @@
 <?php
+
+/*
+- addUser (token)
+- delUser (email)
+- chkUser ()
+- login (email, pwd)
+- register (email, pwd)
+*/
 class userManager
 {
     public $conn;
     function __construct()
     {
-    $config = require ("../config.php");
+    $this->config = require ("../config.php");
         try {
-            $this->conn = new PDO("mysql:host=" . $config["user"]["mysql"]["host"] . ";dbname=" . $config["user"]["mysql"]["databasename"], $config["user"]["mysql"]["username"], $config["user"]["mysql"]["passphrase"]);
+            $this->conn = new PDO("mysql:host=" . $this->config["user"]["mysql"]["host"] . ";dbname=" . $this->config["user"]["mysql"]["databasename"], $this->config["user"]["mysql"]["username"], $this->config["user"]["mysql"]["passphrase"]);
+            $this->redis = new Redis();
+            $this->redis->connect($this->config["redis"]["host"], $this->config["redis"]["port"]);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             
@@ -27,30 +37,42 @@ class userManager
         $schema = $this->getSchema();
         $this->conn->exec($schema);
     }
-    
-    function addMailUser ($name, $pwd, $email, $visibleEmail = 0) {
-        $statement = $this->conn->prepare("INSERT INTO dists (name, pwd, email, visibleEmail) VALUES (?, PASSWORD (?), ?, ?)");
-        $statement->execute(array($name, $pwd, $email, $visibleEmail));
+
+    function struuid($entropy) {
+        $s=uniqid("",$entropy);
+        $num= hexdec(str_replace(".","",(string)$s));
+        $index = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $base= strlen($index);
+        $out = '';
+        for($t = floor(log10($num) / log10($base)); $t >= 0; $t--) {
+            $a = floor($num / pow($base,$t));
+            $out = $out.substr($index,$a,1);
+            $num = $num-($a*pow($base,$t));
+        }
+        return $out;
     }
-    
-    function delMailUserByName ($name) {
-        $statement = $this->conn->prepare("DELETE FROM dists WHERE name = ?");
-        $statement->execute(array($name));
+
+    function addUser($token) {
+        //$sql = $this->conn->prepare("INSERT INTO user (email, pwd) VALUES (?, ?)");
     }
-    
-    function delMailUserByEmail ($email) {
-        $statement = $this->conn->prepare("DELETE FROM dists WHERE email = ?");
-        $statement->execute(array($email));
-    }
-    
-    function chkUser ($nameOrEmail, $pwd) {
-        $statement = $this->conn->prepare("SELECT COUNT(*) FROM dists WHERE (email = ? or name = ?) and pwd = PASSWORD (?)");
-        $statement->execute(array($nameOrEmail, $nameOrEmail, $pwd));
-        return $statement->fetchColumn() > 0;
+
+    function register($email, $pwd) {
+        $sql=$this->conn->prepare("SELECT email FROM user WHERE email=?");
+        $sql->execute(array($email));
+        if ($sql->rowCount() > 0) {
+            echo "user already exists";
+            return;
+        }
+        $load = json_encode (["email" => $email, "pwd" => password_hash($pwd, PASSWORD_DEFAULT)]);
+        $uuid = $this->struuid(true);
+        $this->redis->set($uuid, $load, $this->config["redis"]["timeout"]);
+        echo "send mail to $email with code $uuid";
     }
 
 }
 
 $v = new userManager();
 $v->initDB();
+$v->register("verf@web-utils.eu", "password");
+
 ?>
